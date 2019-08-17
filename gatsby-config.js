1f603c253362ad9c4e4d0811fa329aeddf0ae477
ingvijonasson <contact@ingvijonasson.com>
2019-08-17 08:29:37 +0200

    Clean up config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ingvi`,
+    description: `TODO: description here`,
+    author: `@_ingvi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ingvi`,
+        short_name: `ingvi`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
